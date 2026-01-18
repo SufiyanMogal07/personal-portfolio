7a46a73d86bda7b19531641394b4c82f5c6c48d2
Sufiyan Mogal <sufiyanyaseenmogal@gmail.com>
2026-01-18 13:56:47 +0530

    removed cursor follower due to bug issue

--- app/ClientProvider.tsx
+++ app/ClientProvider.tsx
@@ -10,14 +10,14 @@ export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
 
-  const cursorRef = useRef<HTMLDivElement>(null);
+  // const cursorRef = useRef<HTMLDivElement>(null);
 
-  const handleMouseMove = (e: React.MouseEvent) => {
-    if (cursorRef.current) {
-      cursorRef.current.style.left = `${e.clientX - 10}px`;
-      cursorRef.current.style.top = `${e.clientY - 10}px`;
-    }
-  };
+  // const handleMouseMove = (e: React.MouseEvent) => {
+  //   if (cursorRef.current) {
+  //     cursorRef.current.style.left = `${e.clientX - 10}px`;
+  //     cursorRef.current.style.top = `${e.clientY - 10}px`;
+  //   }
+  // };
 
   useEffect(() => {
     if (isDropDownOpen) {
@@ -32,7 +32,8 @@ export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className="relative" onMouseMove={handleMouseMove}>
+    <div className="relative" >
+    {/* onMouseMove={handleMouseMove} */}
       <div
         className={`min-h-screen relative transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
@@ -52,10 +53,10 @@ export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
         <MobileNavigation onClose={() => setIsDropDownOpen(false)} />
       )}
 
-      <div
+      {/* <div
         ref={cursorRef}
         className={`fixed z-60 w-5 h-5 rounded-full bg-purple-600/50 dark:bg-white/60 pointer-events-none transition-all duration-900 ease-in-out`}
-      />
+      /> */}
     </div>
   );
 };
